14b00fb4f01a05e59a41690ff2c28eaaa02a76ad
ZacML <isaquem673@gmail.com>
2026-03-28 23:09:34 -0300

    ajuste na tab bar do dashboard

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.scss']
 })
